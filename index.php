f23bf1c59bd5849775138bb2d3e053416927cd05
mrzljak_ines <mrzljakines@gmail.com>
2023-03-03 22:07:59 +0100

    inc mrz

--- index.php
+++ index.php
@@ -3,25 +3,75 @@ $title = "Logic Lane - AI rješenja";
 include "header.php";
 ?>
 
-<div class="container col-xxl-8 px-4 py-5">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div class="col-10 col-sm-8 col-lg-6">
-            <img src="images/logo/logiclane-nobcg.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="900" loading="lazy">
-        </div>
-        <div class="col-lg-6">
-            <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-            <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-                popular front-end open source toolkit, featug Sass variables and mixins, responsive grid system,
-                extensive prebuilt components, and powerful JavaScript plugins.</p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+<div class="hero-container">
+    <div class="hero-content">
+        <div class="hero-info">
+            <h1 class="hero-title">
+                Lorem ipsum dolor sit amet, consectetur adipiscing <br> elit.
+            </h1>
+            <div class="btns">
+                <button class="btn-contact"><a href="contact.php" class="contact-hero">Contact us</a></button>
+                <button class="btn-learn-more"><a href="index.php#services" class="learn-more-hero">Learn
+                        more</a></button>
             </div>
+        </div>
+        <div class="hero-graphic">
+            <img class="hero-img" src="images/logo/logiclane-nobcg.png" alt="">
         </div>
     </div>
 </div>
-<div style="height:1080px;background-color:black"></div>
-<div style="height:5000px">
+<div class="about-us-container">
+    <div class="about-us-content">
+        <div class="video-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1104.305" height="1124.332" viewBox="0 0 1104.305 1124.332">
+                <g id="Exclusion_1" data-name="Exclusion 1" transform="translate(-329.467 -245.353)" fill="none">
+                    <path
+                        d="M424.253,1369.684h0l-.1-151.312L1176.73,748.113l-753.1-367.5.531,837.754-94.507,59.055-.184-942.757,94.16,45.948-.086-135.265,1010.23,493.515L424.254,1369.684Z"
+                        stroke="none" />
+                    <path
+                        d="M 425.7518920898438 1366.97900390625 L 1430.683349609375 739.028076171875 L 425.0426940917969 247.7555999755859 L 425.1264343261719 379.6806640625 L 1177.38818359375 746.76513671875 L 1179.819580078125 747.9515991210938 L 1177.525146484375 749.3853149414063 L 425.6582641601563 1219.20361328125 L 425.7518920898438 1366.97900390625 M 331.1500549316406 1274.72119140625 L 422.6571960449219 1217.541259765625 L 422.1276245117188 381.5553588867188 L 330.9670715332031 337.0713500976563 L 331.1500549316406 1274.72119140625 M 424.253173828125 1369.684326171875 L 424.2536010742188 1369.683959960938 L 424.1577453613281 1218.372436523438 L 1176.730346679688 748.1131591796875 L 423.6270141601563 380.6180114746094 L 424.1577453613281 1218.372436523438 L 329.6506042480469 1277.427001953125 L 329.4665832519531 334.6701354980469 L 423.6270141601563 380.6180114746094 L 423.5411682128906 245.3527221679688 L 1433.771484375 738.8673095703125 L 424.2536010742188 1369.683959960938 L 424.253173828125 1369.684326171875 Z"
+                        stroke="none" fill="#0ab59d" />
+                </g>
+            </svg>
+        </div>
+        <div class="about-us-col">
+            <h2 class="about-us-title">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </h2>
+            <p class="about-us-info">
+                Praesent blandit neque dui, sed elementum ligula pretium at. Sed ultrices, libero non dignissim
+                tincidunt,
+                sapien ante commodo orci, nec semper magna neque sit amet nunc. Pellentesque quis nibh eget ante
+                facilisis
+                dictum. Fusce id quam justo. Aliquam interdum faucibus fermentum. Proin et eleifend elit. <br><br>
+                <button class="btn-contact"><a href="contact.php" class="contact-hero">Contact us</a></button>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="services-container">
+
+    <div class="mt-4">
+        <h2 class="services-title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
+        <div class="row image-row mx-5">
+            <div class="col-lg-4 col-md-12 mb-4 mb-lg-0 image-column">
+                <div class="card-box h-30"></div>
+                <div class="card-box h-70"></div>
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0 image-column">
+                <div class="card-box h-70"></div>
+                <div class="card-box h-30"></div>
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0 image-column">
+                <div class="card-box h-30"></div>
+                <div class="card-box h-70"></div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
