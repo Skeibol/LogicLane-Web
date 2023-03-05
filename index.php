e279908ab9f3d46c7f53b9cd0d3ef0db90f71e35
mrzljak_ines <mrzljakines@gmail.com>
2023-03-05 15:28:21 +0100

    ne znam sta sam napravila novo

--- index.php
+++ index.php
@@ -25,13 +25,17 @@ include "header.php";
         <div class="video-container">
             <svg xmlns="http://www.w3.org/2000/svg" width="1104.305" height="1124.332" viewBox="0 0 1104.305 1124.332">
                 <g id="Exclusion_1" data-name="Exclusion 1" transform="translate(-329.467 -245.353)" fill="none">
-                    <path d="M424.253,1369.684h0l-.1-151.312L1176.73,748.113l-753.1-367.5.531,837.754-94.507,59.055-.184-942.757,94.16,45.948-.086-135.265,1010.23,493.515L424.254,1369.684Z" stroke="none" />
-                    <path d="M 425.7518920898438 1366.97900390625 L 1430.683349609375 739.028076171875 L 425.0426940917969 247.7555999755859 L 425.1264343261719 379.6806640625 L 1177.38818359375 746.76513671875 L 1179.819580078125 747.9515991210938 L 1177.525146484375 749.3853149414063 L 425.6582641601563 1219.20361328125 L 425.7518920898438 1366.97900390625 M 331.1500549316406 1274.72119140625 L 422.6571960449219 1217.541259765625 L 422.1276245117188 381.5553588867188 L 330.9670715332031 337.0713500976563 L 331.1500549316406 1274.72119140625 M 424.253173828125 1369.684326171875 L 424.2536010742188 1369.683959960938 L 424.1577453613281 1218.372436523438 L 1176.730346679688 748.1131591796875 L 423.6270141601563 380.6180114746094 L 424.1577453613281 1218.372436523438 L 329.6506042480469 1277.427001953125 L 329.4665832519531 334.6701354980469 L 423.6270141601563 380.6180114746094 L 423.5411682128906 245.3527221679688 L 1433.771484375 738.8673095703125 L 424.2536010742188 1369.683959960938 L 424.253173828125 1369.684326171875 Z" stroke="none" fill="#0ab59d" />
+                    <path
+                        d="M424.253,1369.684h0l-.1-151.312L1176.73,748.113l-753.1-367.5.531,837.754-94.507,59.055-.184-942.757,94.16,45.948-.086-135.265,1010.23,493.515L424.254,1369.684Z"
+                        stroke="none" />
+                    <path
+                        d="M 425.7518920898438 1366.97900390625 L 1430.683349609375 739.028076171875 L 425.0426940917969 247.7555999755859 L 425.1264343261719 379.6806640625 L 1177.38818359375 746.76513671875 L 1179.819580078125 747.9515991210938 L 1177.525146484375 749.3853149414063 L 425.6582641601563 1219.20361328125 L 425.7518920898438 1366.97900390625 M 331.1500549316406 1274.72119140625 L 422.6571960449219 1217.541259765625 L 422.1276245117188 381.5553588867188 L 330.9670715332031 337.0713500976563 L 331.1500549316406 1274.72119140625 M 424.253173828125 1369.684326171875 L 424.2536010742188 1369.683959960938 L 424.1577453613281 1218.372436523438 L 1176.730346679688 748.1131591796875 L 423.6270141601563 380.6180114746094 L 424.1577453613281 1218.372436523438 L 329.6506042480469 1277.427001953125 L 329.4665832519531 334.6701354980469 L 423.6270141601563 380.6180114746094 L 423.5411682128906 245.3527221679688 L 1433.771484375 738.8673095703125 L 424.2536010742188 1369.683959960938 L 424.253173828125 1369.684326171875 Z"
+                        stroke="none" fill="#0ab59d" />
                 </g>
             </svg>
         </div>
         <div class="about-us-col">
-            <h2 class="about-us-title">
+            <h2 class="about-us-title" id="about-us">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </h2>
             <p class="about-us-info">
@@ -83,25 +87,31 @@ include "header.php";
 
 <!-- Gallery -->
 <div class="services-container">
-    <h2 class="services-title">
+    <h2 class="services-title" id="services">
         Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
     <div class="row">
         <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Boat on Calm Water" />
 
-            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" />
+            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape" />
         </div>
 
         <div class="col-lg-4 mb-4 mb-lg-0">
-            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
+            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Mountains in the Clouds" />
 
-            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Boat on Calm Water" />
         </div>
 
         <div class="col-lg-4 mb-4 mb-lg-0">
-            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+            <img class="card-box h-30" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Waves at Sea" />
 
-            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
+            <img class="card-box h-70" src="images/image.jpeg" class="w-100 shadow-1-strong rounded mb-4"
+                alt="Yosemite National Park" />
         </div>
     </div>
 </div>
@@ -134,7 +144,9 @@ include "header.php";
         <button class="switch-up"> <svg xmlns="http://www.w3.org/2000/svg" width="73" height="72" viewBox="0 0 73 72">
                 <g id="Polygon_9" data-name="Polygon 9" fill="none">
                     <path d="M36.5,0,73,72H0Z" stroke="none" />
-                    <path d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z" stroke="none" fill="#0ab59d" />
+                    <path
+                        d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z"
+                        stroke="none" fill="#0ab59d" />
                 </g>
             </svg>
         </button>
@@ -157,13 +169,15 @@ include "header.php";
         <button class="switch-down"><svg xmlns="http://www.w3.org/2000/svg" width="73" height="72" viewBox="0 0 73 72">
                 <g id="Polygon_10" data-name="Polygon 10" transform="translate(73 72) rotate(-180)" fill="none">
                     <path d="M36.5,0,73,72H0Z" stroke="none" />
-                    <path d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z" stroke="none" fill="#0ab59d" />
+                    <path
+                        d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z"
+                        stroke="none" fill="#0ab59d" />
                 </g>
             </svg>
         </button>
     </div>
     <div class="carousel--right carousel--container carousel--info">
-        <h2 class="carousel--header">Projekt</h2>
+        <h2 class="carousel--header" id="projects">Projekt</h2>
         <p class="carousel--text">Praesent blandit neque dui, sed elementum ligula pretium at. Sed ultrices, libero non
             dignissim tincidunt, sapien ante commodo orci, nec semper magna neque sit amet nunc. Pellentesque quis nibh
             eget ante facilisis dictum. Fusce id quam justo. Aliquam interdum faucibus fermentum. Proin et eleifend
