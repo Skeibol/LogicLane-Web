d3ff9f387e6058ea78a8cd5fd223d4a7037cc83a
Antonio Kezele <akezele35@gmail.com>
2023-03-07 16:51:52 +0100

    ca

--- contact-us.php
+++ contact-us.php
@@ -1,34 +1,26 @@
 <?php
 $title = "Logic Lane - AI rješenja";
 include "header.php";
-/**
- * This example shows how to handle a simple contact form safely.
- */
 
-//Import PHPMailer class into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-# start session handling.
 
 if (!isset($msg)) {
     $msg = '';
 }
+
+
 //Don't run this unless we're handling a form submission
-if (array_key_exists('email', $_POST)) {
+if (array_key_exists('email', $_POST) && $_POST['med'] == "") {
     date_default_timezone_set('Etc/UTC');
 
     require 'vendor/autoload.php';
-
-    //Create a new PHPMailer instance
     $mail = new PHPMailer(true);
 
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
     $mail->isSMTP();
     $mail->SMTPAuth = true;
-    //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
-    //See other examples for how to use a remote server such as gmail
     $mail->Host = "in-v3.mailjet.com";
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
@@ -36,17 +28,9 @@ if (array_key_exists('email', $_POST)) {
     $mail->Username = "44b23ce71ec4c516f6b302978d8ec0e0";
     $mail->Password = "135fb6d96545b09a58822f1a604d7b3e";
 
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
+
     $mail->setFrom('akezele35@gmail.com', 'Antonio Kezele');
-
-    //Fall back to a fixed address if dept selection is invalid or missing
     $mail->addAddress('akezele35@gmail.com');
-
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
 
 
     if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
@@ -55,14 +39,13 @@ if (array_key_exists('email', $_POST)) {
         $mail->isHTML(false);
         //Build a simple message body
         $mail->Body = <<<EOT
-Email: {$_POST['email']}
-Name: {$_POST['name']}
-Message: {$_POST['message']}
-EOT;
+        Email: {$_POST['email']}
+        Name: {$_POST['name']}
+        Message: {$_POST['message']}
+        EOT;
         //Send the message, check for errors
         if (!$mail->send()) {
             //The reason for failing to send will be in $mail->ErrorInfo
-            //but it's unsafe to display errors directly to users - process the error, log it on your server.
             $msg = 'Sorry, something went wrong. Please try again later.';
         } else {
             $msg = 'Message sent! Thanks for contacting us.';
@@ -82,13 +65,15 @@ EOT;
     <h2 class="form--text" id="#form-text">Send us a message</h2>
     <form class="form--action" method="post" action="#form-text">
         <label for="name">Your name:</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" pattern="[a-zA-Z ]{1,}" required>
 
         <label for="email">Your email:</label>
         <input type="email" id="email" name="email">
 
         <label for="message">Your message:</label>
         <textarea id="message" name="message" style="height:200px" placeholder=".." required></textarea>
+
+        <input type="text" id="med" name="med" style="display:none" value="">
 
         <button class="btn-contact btn-send" type="submit" name="submit" id="contact-submit"><a
                 class="contact-hero">Send</a></button>
