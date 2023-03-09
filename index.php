b37fa5c8912ccd8d8da690f864ac287055a6201b
IncMrz <mrzljakines@gmail.com>
2023-03-09 17:16:58 +0100

    glupostice

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "header.php";
             <p class="hero-subtitle">Team of AI developers providing custom solutions for your project needs
             </p>
             <div class="btns">
-                <button class="btn-contact"><a href="contact-us.php">Contact us</a></button>
+                <button class="btn-contact"><a class="invert" href="contact-us.php">Contact us</a></button>
                 <button class="btn-learn-more"><a href="index.php#services" class="learn-more-hero">Learn
                         more</a></button>
             </div>
@@ -33,30 +33,31 @@ include "header.php";
         </div>
     </div>
     <div class="bcg-hero">
-        <img class="svg--fill" src="images/top-hero.svg" alt="" class="bcg">
+
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1926.53 224">
+            <defs>
+            </defs>
+            <polygon class="cls-1-hero" points="1926 224 0 224 0 0 1926 144 1926 224" />
+            <line class="cls-2-bg" x1="1.91" y1="40.95" x2="1926.5" y2="186.5" />
+        </svg>
+
     </div>
 </div>
 
 <!-- ABOUT US -->
 <div class="about-us-container" id="about-us">
-    <div class="about-us-content">
+    <div class="about-us-content row">
         <div class="video-container">
             <!-- VIDEO SVG -->
             <svg xmlns="http://www.w3.org/2000/svg" width="1188.493" height="1119.957" viewBox="0 0 1188.493 1119.957">
                 <g id="Group_17" data-name="Group 17" transform="translate(328.772 -899.426)">
-                    <g id="Polygon_2" data-name="Polygon 2"
-                        transform="matrix(0.848, -0.53, 0.53, 0.848, -328.772, 1420.131)" fill="none">
+                    <g id="Polygon_2" data-name="Polygon 2" transform="matrix(0.848, -0.53, 0.53, 0.848, -328.772, 1420.131)" fill="none">
                         <path d="M388.5,0,777,622H0Z" stroke="none" />
-                        <path class="svg--fill"
-                            d="M 388.4999084472656 2.8314208984375 L 2.70538330078125 620.5 L 774.2946166992188 620.5 L 388.4999084472656 2.8314208984375 M 388.4999084472656 0 L 777 622 L 0 622 L 388.4999084472656 0 Z"
-                            stroke="none" fill="#0ab59d" />
+                        <path class="svg--fill" d="M 388.4999084472656 2.8314208984375 L 2.70538330078125 620.5 L 774.2946166992188 620.5 L 388.4999084472656 2.8314208984375 M 388.4999084472656 0 L 777 622 L 0 622 L 388.4999084472656 0 Z" stroke="none" fill="#0ab59d" />
                     </g>
-                    <g id="Polygon_4" data-name="Polygon 4"
-                        transform="matrix(0.848, -0.53, 0.53, 0.848, -318.772, 1390.131)" fill="none">
+                    <g id="Polygon_4" data-name="Polygon 4" transform="matrix(0.848, -0.53, 0.53, 0.848, -318.772, 1390.131)" fill="none">
                         <path d="M463,0,926,742H0Z" stroke="none" />
-                        <path class="svg--fill"
-                            d="M 462.9998779296875 2.83355712890625 L 2.7041015625 740.5 L 923.2958984375 740.5 L 462.9998779296875 2.83355712890625 M 462.9998779296875 0 L 926 742 L 0 742 L 462.9998779296875 0 Z"
-                            stroke="none" fill="#0ab59d" />
+                        <path class="svg--fill" d="M 462.9998779296875 2.83355712890625 L 2.7041015625 740.5 L 923.2958984375 740.5 L 462.9998779296875 2.83355712890625 M 462.9998779296875 0 L 926 742 L 0 742 L 462.9998779296875 0 Z" stroke="none" fill="#0ab59d" />
                     </g>
                 </g>
             </svg>
@@ -73,7 +74,7 @@ include "header.php";
                 sapien ante commodo orci, nec semper magna neque sit amet nunc. Pellentesque quis nibh eget ante
                 facilisis
                 dictum. Fusce id quam justo. Aliquam interdum faucibus fermentum. Proin et eleifend elit. <br><br>
-                <button class="btn-contact invert"><a class="invert" href="contact.php">Contact us</a></button>
+                <button class="btn-contact"><a class="invert" href="contact-us.php">Contact us</a></button>
             </p>
         </div>
     </div>
@@ -153,42 +154,23 @@ include "header.php";
     </div>
 </div>
 
-<!-- TECHNOLOGY -->
-<div class="tech-container">
-    <div class="tech-content">
-        <div class="tech-items">
-            <div class="tech-item">
-                <img src="images/tf.png" alt="" class="tf">
-            </div>
-            <div class="tech-item">
-                <img src="images/tf.png" alt="" class="tf">
-            </div>
-            <div class="tech-item">
-                <img src="images/tf.png" alt="" class="tf">
-            </div>
-            <div class="tech-item">
-                <img src="images/tf.png" alt="" class="tf">
-            </div>
-            <div class="tech-item">
-                <img src="images/tf.png" alt="" class="tf">
-            </div>
-        </div>
-    </div>
-
-</div>
-<div class="bcg-1">
-    <img class="svg--fill" src="images/logo/bg-line-top.svg" alt="">
-</div>
 
 <!-- CAROUSEL -->
+<div class="bcg-1">
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1917.54 222.41">
+        <defs>
+
+        </defs>
+        <polyline class="cls-1-bg" points="0.54 30.28 0.54 222.41 1917.54 222.41" />
+        <line class="cls-2-bg" x1="0.04" y1="0.41" x2="1917.04" y2="192.54" />
+    </svg>
+</div>
 <div class="carousel" id="projects">
     <div class="carousel--left carousel--container">
         <button class="switch-up"> <svg xmlns="http://www.w3.org/2000/svg" width="73" height="72" viewBox="0 0 73 72">
                 <g id="Polygon_9" data-name="Polygon 9" fill="none">
                     <path d="M36.5,0,73,72H0Z" stroke="none" />
-                    <path class="svg--fill"
-                        d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z"
-                        stroke="none" fill="#0ab59d" />
+                    <path class="svg--fill" d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z" stroke="none" fill="#0ab59d" />
                 </g>
             </svg>
         </button>
@@ -211,9 +193,7 @@ include "header.php";
         <button class="switch-down"><svg xmlns="http://www.w3.org/2000/svg" width="73" height="72" viewBox="0 0 73 72">
                 <g id="Polygon_10" data-name="Polygon 10" transform="translate(73 72) rotate(-180)" fill="none">
                     <path d="M36.5,0,73,72H0Z" stroke="none" />
-                    <path class="svg--fill"
-                        d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z"
-                        stroke="none" fill="#0ab59d" />
+                    <path class="svg--fill" d="M 36.5 3.317390441894531 L 2.442146301269531 70.5 L 70.55785369873047 70.5 L 36.5 3.317390441894531 M 36.5 0 L 73 72 L 0 72 L 36.5 0 Z" stroke="none" fill="#0ab59d" />
                 </g>
             </svg>
         </button>
@@ -224,16 +204,96 @@ include "header.php";
     </div>
 </div>
 <div class="bcg">
-    <img src="images/logo/bg-line-bottom.svg" alt="">
+    <!-- <img src="images/logo/bg-line-bottom.svg" alt=""> -->
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1917.54 218.41">
+        <polyline class="cls-1-bg" points="0.54 188.67 0.54 0 1917.54 0" />
+        <line class="cls-2-bg" x1="0.04" y1="218" x2="1917.04" y2="29.33" />
+    </svg>
 </div>
 
-<!-- QUOTE -->
-<h1 class="quote">
-    <strong style="color:var(--color-primary)">&#8222;</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    <strong style="color:var(--color-primary)">&#8221;</strong>
-</h1>
+
+<!-- TECHNOLOGY -->
+<div class="tech-container">
+    <div class="tech-content">
+        <div class="tech-items">
+        <h2 class="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
+            <div class="row row-tech">
+                <div class="tech-item">
+                    <img src="images/technologies/c-sharp.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/cplusplus.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/php.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/python.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/pandas.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/keras.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/scikit.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/pytorch.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/tf.png" alt="" class="tf">
+                </div>
+            </div>
+            <div class="row row-tech">
+            <div class="tech-item">
+                    <img src="images/technologies/js.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/node.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/react.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/bootstrap.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/css.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/sql.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/blender.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/unity.png" alt="" class="tf">
+                </div>
+                <div class="tech-item">
+                    <img src="images/technologies/acad.png" alt="" class="tf">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
 
 <!-- FAQ -->
+
+<div class="bcg-1">
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1917.54 222.41">
+        <defs>
+
+        </defs>
+        <polyline class="cls-1-bg" points="0.54 30.28 0.54 222.41 1917.54 222.41" />
+        <line class="cls-2-bg" x1="0.04" y1="0.41" x2="1917.04" y2="192.54" />
+    </svg>
+</div>
 <div class="FAQ--container" id="FAQ">
     <h1>FAQ</h1>
     <button class="FAQ--section">
@@ -297,9 +357,20 @@ include "header.php";
         </p>
     </div>
 </div>
+<div class="bcg">
+    <!-- <img src="images/logo/bg-line-bottom.svg" alt=""> -->
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1917.54 218.41">
+        <polyline class="cls-1-bg" points="0.54 188.67 0.54 0 1917.54 0" />
+        <line class="cls-2-bg" x1="0.04" y1="218" x2="1917.04" y2="29.33" />
+    </svg>
+</div>
 
 
-
+<!-- QUOTE -->
+<h1 class="quote">
+    <strong style="color:var(--color-primary)">&#8222;</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <strong style="color:var(--color-primary)">&#8221;</strong>
+</h1>
 
 
 
--- js/navScroller.js
+++ js/navScroller.js
@@ -15,12 +15,30 @@ document.getScroll = function() {
 var headerHeight = document.getElementsByClassName('hero-container')[0].offsetHeight
 var navbarHeight = document.getElementsByClassName('navbar')[0].offsetHeight
 $(window).on('scroll',function() {
-    if(document.getScroll()[1] > headerHeight - navbarHeight - 100){
+    if(document.getScroll()[1] > headerHeight - navbarHeight - 110){
         $(".navbar").addClass("scrolled")
 
     }
     else{
         $(".navbar").removeClass("scrolled")
 
+    }
+});
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+if ($('.navbar-toggler').attr('aria-expanded') === "true") {
+        navLinks.forEach((l) => {
+            l.addEventListener('click', () => { bsCollapse.toggle() })
+        })
+    }
+
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");               
+    var _opened = $navbar.hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {      
+        $navbar.collapse('hide');
     }
 });
